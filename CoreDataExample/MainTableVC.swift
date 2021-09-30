55e2281eeeb611efc4effe3daaba4ba3ca5e9a53
Murat Menzilci <menzilcim>
2021-09-30 15:05:15 +0300

    TableView created!

---
+++ CoreDataExample/MainTableVC.swift
@@ -0,0 +1,34 @@
+//
+//  MainTableVC.swift
+//  CoreDataExample
+//
+//  Created by Murat Menzilci on 30.09.2021.
+//
+
+import UIKit
+
+class MainTableVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello World"
+        return cell
+    }
+
+}
---
+++ CoreDataExample/MyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyTableViewCell.swift
+//  CoreDataExample
+//
+//  Created by Murat Menzilci on 30.09.2021.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
